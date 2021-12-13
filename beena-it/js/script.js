cd73c54731e37366549c69174f1fed3d5cbee581
Pooja <poojaruby005@gmail.com>
2021-12-13 12:30:15 +0530

    added

--- beena-it/js/script.js
+++ beena-it/js/script.js
@@ -57,3 +57,83 @@ jQuery("[data-color]").each(function () {
 jQuery("[data-bgcolor]").each(function () {
 	jQuery(this).css('background-color', jQuery(this).attr('data-bgcolor'));
 });
+
+
+
+$(document).ready(function() {
+    // Variable to hold request
+    var request;
+    // Bind to the submit event of our form
+    $("#sky-form").submit(function(event) {
+        // Prevent default posting of form - put here to work in case of errors
+        event.preventDefault();
+        // debugger;
+        var _name = $("#name").val();
+        var _email = $("#email").val();
+        var _phone = $("#phone").val();
+        var _subject = $("#subject").val();
+        var _message = $("#message").val();
+
+        if(_name == null || _name == "" || 
+            _email == null || _email == "" ||
+            _phone == null || _phone == "" || 
+            _subject == null || _subject == "" || 
+            _message == null || _message == "") {
+            return;
+        }
+
+        // Abort any pending request
+        if (request) {
+            request.abort();
+        }
+        // setup some local variables
+        var $form = $(this);
+
+        // Let's select and cache all the fields
+        var $inputs = $form.find("input, select, button, textarea");
+
+        // Serialize the data in the form
+        var serializedData = $form.serialize();
+
+        // Let's disable the inputs for the duration of the Ajax request.
+        // Note: we disable elements AFTER the form data has been serialized.
+        // Disabled form elements will not be serialized.
+        // $inputs.prop("disabled", true);
+
+        // Fire off the request to /form.php
+        request = $.ajax({
+            url: "mail-success.php",
+            type: "post",
+            data: serializedData
+        });
+
+        // Callback handler that will be called on success
+        request.done(function (response, textStatus, jqXHR) {
+            $("#sky-form").trigger("reset");
+            $("#exampleModalCenter").modal("toggle");
+            $("#thankModalCenter").modal("show");
+        });
+
+        // Callback handler that will be called on failure
+        request.fail(function (jqXHR, textStatus, errorThrown) {
+            // Log the error to the console
+            console.error(
+                "The following error occurred: "+
+                textStatus, errorThrown
+            );
+        });
+
+        // Callback handler that will be called regardless
+        // if the request failed or succeeded
+        request.always(function () {
+            // Reenable the inputs
+            $inputs.prop("disabled", false);
+        });
+
+    });
+
+    $('#myModal').modal({
+     backdrop: 'static',
+     keyboard: false
+    });
+});
