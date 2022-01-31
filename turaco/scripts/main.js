65615fce12a50c12716ed220c27d15a9b55919d0
Pooja <poojaruby005@gmail.com>
2022-01-31 11:27:21 +0530

    added

--- turaco/scripts/main.js
+++ turaco/scripts/main.js
@@ -161,5 +161,48 @@ $(document).on('ready', function () {
   });
 
 });
+//slick slider
+$(document).on('ready', function () {
+
+  $('.prom-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode:true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode:true
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      //settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+});
+
 
 
